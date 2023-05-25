1617cc2c304fa01f5c495a238ccde7982ab2a789
Russkikh Konstanin <russ.kostua83@gmail.com>
2023-05-25 22:07:09 +0300

    add test_github

---
+++ pytest.ini.py
@@ -0,0 +1,12 @@
+# import results as results
+#
+# import allure
+# import pytest
+#
+# [pytest]
+#
+# addopts =
+#
+#     --clean-alluredir
+#
+#     --alluredir=allure-results
---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+# import allure
+# import pytest
+#
+# [pytest]
+# addopts = --alluredir=allure-results
